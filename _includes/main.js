830740f3ebab73aad934c790afb03c819ee7009d
rodincode <70000245+rodincode@users.noreply.github.com>
2021-01-15 13:53:39 +0530

    Create main.js

---
+++ _includes/main.js
@@ -0,0 +1,11 @@
+var config = {
+    apiKey: "YOUR_API_KEY",
+    authDomain: "Your_AuthDomain",
+    databaseURL: "YOUR_DATABAE_URL",
+    projectId: "YOUR_PROJECT_ID",
+    storageBucket: "YOUR_STORAGE_BUCKET",
+    messagingSenderId: "YOUR_SENDER_ID",
+    appId: "YOUR_APP_ID"
+};
+firebase.initializeApp(config);
+    
